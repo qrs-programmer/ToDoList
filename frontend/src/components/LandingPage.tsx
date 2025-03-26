370919d6ba0935fd43c4188b0416543518f950b9
qrs-programmer <q.reilly.shannon@gmail.com>
2025-03-26 19:10:54 -0400

    Created Simple Landing Page UI

---
+++ frontend/src/components/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LandingPage.css";
+
+const LandingPage: React.FC = () => {
+  return (
+    <div className="container">
+      <button className="button">Get Started</button>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <div className="brand">ToDoList</div>
+    </nav>
+  );
+};
+
+export default Navbar;
