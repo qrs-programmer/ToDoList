378542fb97a0a07381280977a04c357f98592830
qrs-programmer <q.reilly.shannon@gmail.com>
2025-05-20 19:54:17 -0400

    Added Delete Operation

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -26,4 +26,20 @@ router.post("/", async (req: any, res: any) => {
     }
   });
 
+  // Delete a task
+  router.delete("/:id", async (req: any, res:any) => {
+    try {
+        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+
+        if (!deletedTask){
+          return res.status(404).json({message:"Task not found!"});
+        }
+
+        res.status(200).json({message: "Task deleted successfully", task: deletedTask});
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error deleting task"});
+      }
+  });
+
   export default router;
