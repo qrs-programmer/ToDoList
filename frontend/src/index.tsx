bb0dd6d9df21fe0dbb87a018c246bd372693f24d
qrs-programmer <q.reilly.shannon@gmail.com>
2025-10-14 13:46:40 -0400

    Change callback url for auth0

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     domain={process.env.REACT_APP_AUTH0_DOMAIN!}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_uri: window.location.origin + "/callback",
       audience: process.env.REACT_APP_AUTH0_AUDIENCE,
     }}
   >
